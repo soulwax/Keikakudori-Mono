56509d6e9fb60e5a3c925e5ee4df4f965aac1260
soulwax <soulwax-95@protonmail.com>
2025-04-02 11:18:36 +0200

    Add initial project files and structure for Keika game  for mac development specifically

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Keika
+{
+
+    /// <summary>
+    /// This is the main entry point for the application.
+    /// </summary>
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Console.WriteLine("Hello, MonoGame!");
+            Console.WriteLine("This is a simple MonoGame program.");
+
+            using var game = new Keikakudori();
+            game.Run();
+            Console.WriteLine("Game has started. Press any key to exit...");
+            Console.WriteLine("Exiting...");
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ src/input/InputHandler.cs
@@ -0,0 +1,27 @@
+using Microsoft.Xna.Framework.Input;
+namespace keika
+{
+
+    public class InputHandler
+    {
+        private KeyboardState _currentKeyboardState;
+        private KeyboardState _previousKeyboardState;
+
+        public InputHandler()
+        {
+            _currentKeyboardState = Keyboard.GetState();
+            _previousKeyboardState = _currentKeyboardState;
+        }
+
+        public void Update()
+        {
+            _previousKeyboardState = _currentKeyboardState;
+            _currentKeyboardState = Keyboard.GetState();
+        }
+
+        public bool IsKeyPressed(Keys key)
+        {
+            return _currentKeyboardState.IsKeyDown(key) && _previousKeyboardState.IsKeyUp(key);
+        }
+    }
+}
